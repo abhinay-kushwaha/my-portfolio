0f02e0ba3b91205054f31b80d3bf341d97bd919c
Abhinay kushwaha <abhinaythresia@gmail.com>
2024-07-07 23:11:07 +0530

    working on skills

--- frontend/src/Components/Portfolio/PortfolioTwo.jsx
+++ frontend/src/Components/Portfolio/PortfolioTwo.jsx
@@ -2,61 +2,61 @@ import React from 'react'
 
 const PortfolioTwo = () => {
   return (
-    <div className='relative -top-56'>
+    <div className='relative -top-44'>
       <div className='flex flex-col gap-24'>
 
-       <div  className='p-32  rounded-xl hover:scale-105 ease-in duration-300'
+       <div  className='md:p-32 sm:p-20 p-14  rounded-xl hover:scale-105 ease-in duration-300'
        style={{ 
       backgroundImage: "url('../src/assets/portfolio-bg-1.jpg')",
       backgroundSize: "cover",
        backgroundRepeat: "no-repeat", 
        backgroundPosition: "center",
       }}>
-          <div className=' w-96 flex flex-col gap-6'>
-              <h1 className='text-red-500 font-semibold text-xl'>YouTube</h1>
-              <h2 className='font-bold text-4xl text-white'>Web Application for Desiverse</h2>
+          <div className=' w-96 flex flex-col gap-3 md:gap-6'>
+              <h1 className='text-red-500 font-semibold text-lg md:text-xl'>YouTube</h1>
+              <h2 className='font-bold text-2xl md:text-4xl text-white'>Web Application for Desiverse</h2>
               <a className='font-semibold flex justify-start items-center text-white' href="#">VIEW PROJECT <span>👉</span></a>
             </div>
        </div>
 
-       <div  className='p-32  rounded-xl hover:scale-105 ease-in-out duration-300 flex justify-end '
+       <div  className='md:p-32 sm:p-20 p-14  rounded-xl hover:scale-105 ease-in-out duration-300 flex justify-end '
        style={{ 
       backgroundImage: "url('../src/assets/portfolio-bg-2.jpg')",
       backgroundSize: "cover",
        backgroundRepeat: "no-repeat", 
        backgroundPosition: "center",
       }}>
-          <div className=' w-96 flex flex-col   gap-6'>
-              <h1 className='text-red-500 font-semibold text-xl'>Vimeo</h1>
-              <h2 className='font-bold text-4xl text-white'>Web Application for Desiverse</h2>
+          <div className=' w-96 flex flex-col   gap-3 md:gap-6'>
+              <h1 className='text-red-500 font-semibold text-lg md:text-xl'>Vimeo</h1>
+              <h2 className='font-bold text-2xl md:text-4xl text-white'>Web Application for Desiverse</h2>
               <a className='font-semibold flex justify-start items-center text-white' href="#">VIEW PROJECT <span>👉</span></a>
             </div>
        </div>
 
-       <div  className='p-32  rounded-xl hover:scale-105 ease-in-out duration-300'
+       <div  className='md:p-32 sm:p-20 p-14  rounded-xl hover:scale-105 ease-in-out duration-300'
        style={{ 
       backgroundImage: "url('../src/assets/portfolio-bg-3.jpg')",
       backgroundSize: "cover",
        backgroundRepeat: "no-repeat", 
        backgroundPosition: "center",
       }}>
-          <div className=' w-96 flex flex-col gap-6'>
-              <h1 className='text-red-500 font-semibold text-xl'>SoundCloud</h1>
-              <h2 className='font-bold text-4xl text-white'>Web Application for Desiverse</h2>
+          <div className=' w-96 flex flex-col gap-3 md:gap-6'>
+              <h1 className='text-red-500 font-semibold text-lg md:text-xl'>SoundCloud</h1>
+              <h2 className='font-bold text-2xl md:text-4xl text-white'>Web Application for Desiverse</h2>
               <a className='font-semibold flex justify-start items-center text-white' href="#">VIEW PROJECT <span>👉</span></a>
             </div>
        </div>
 
-       <div  className='p-32  rounded-xl hover:scale-105 ease-in-out duration-300  flex justify-end'
+       <div  className='md:p-32 sm:p-20 p-14  rounded-xl hover:scale-105 ease-in-out duration-300  flex justify-end'
        style={{ 
       backgroundImage: "url('../src/assets/portfolio-bg-4.jpg')",
       backgroundSize: "cover",
        backgroundRepeat: "no-repeat", 
        backgroundPosition: "center",
       }}>
-          <div className=' w-96 flex flex-col gap-6'>
-              <h1 className='text-red-500 font-semibold text-xl'>Details</h1>
-              <h2 className='font-bold text-4xl text-white'>Web Application for Desiverse</h2>
+          <div className=' w-96 flex flex-col gap-3 md:gap-6'>
+              <h1 className='text-red-500 font-semibold text-lg md:text-xl'>Details</h1>
+              <h2 className='font-bold text-2xl md:text-4xl text-white'>Web Application for Desiverse</h2>
               <a className='font-semibold flex justify-start items-center text-white' href="#">VIEW PROJECT <span>👉</span></a>
             </div>
        </div>
